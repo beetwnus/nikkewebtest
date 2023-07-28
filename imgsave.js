3dfcf9e5536ddd67b215e3b5775ce7f4e6547cad
beetwnus <131743022+beetwnus@users.noreply.github.com>
2023-07-28 15:18:25 +0800

    Add files via upload

--- imgsave.js
+++ imgsave.js
@@ -1,10 +1,8 @@
 function screenshot(){
     html2canvas(document.getElementById('nikke-images')).then(function(canvas) {
-        document.body.appendChild(canvas);
-        var a = document.createElement('a');
-        a.crossOrigin = "anonymous"; // 設定為匿名模式，處理跨域
-        a.href = canvas.toDataURL("image/png").replace("image/octet-stream");
-        a.download = 'mynikke.png';
-        a.click();
+        var newTab = window.open();
+        newTab.crossOrigin = "anonymous"; // 設定為匿名模式，處理跨域
+        newTab.document.write('<img src="' + canvas.toDataURL("image/png") + '" alt="mynikke.png"/>');
     });
 }
+
