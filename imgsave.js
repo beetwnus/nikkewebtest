8f2f02374c9e3e278b0172bbe4d56a1cf8281b68
beetwnus <131743022+beetwnus@users.noreply.github.com>
2023-07-28 19:39:06 +0800

    Add files via upload

--- imgsave.js
+++ imgsave.js
@@ -7,7 +7,7 @@ function screenshot() {
     saveButton.classList.add('hidden');
     dataButton.classList.add('hidden');
 
-    html2canvas(document.getElementById('main')).then(function (canvas) {
+    html2canvas(document.getElementById('nikke-images')).then(function (canvas) {
         var newTab = window.open();
         newTab.crossOrigin = "anonymous"; // 設定為匿名模式，處理跨域
         newTab.document.write('<img src="' + canvas.toDataURL("image/png") + '" alt="mynikke.png"/>');
