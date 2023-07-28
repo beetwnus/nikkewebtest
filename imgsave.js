65460d83a3fcf4c71936603b1e45cab57dace7e7
beetwnus <131743022+beetwnus@users.noreply.github.com>
2023-07-28 17:12:48 +0800

    Add files via upload

--- imgsave.js
+++ imgsave.js
@@ -19,31 +19,4 @@ function screenshot() {
     saveButton.classList.remove('hidden');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var reverseCheckbox = document.getElementById('reverseCheckbox');
 
-    // 添加复选框状态改变事件监听器
-    reverseCheckbox.addEventListener('change', function () {
-        // 根据复选框的选中状态切换左右键功能
-        if (reverseCheckbox.checked) {
-            document.addEventListener('mousedown', reverseMouseDown, false);
-        } else {
-            document.removeEventListener('mousedown', reverseMouseDown, false);
-        }
-    });
-
-    // 定义一个函数，用于在左右键功能相反状态下执行的操作
-    function reverseMouseDown(event) {
-        // 取消默认右键菜单弹出
-        event.preventDefault();
-
-        // 判断是左键还是右键，并执行相应的操作
-        if (event.button === 0) {
-            // 左键操作（在这里执行右键操作的逻辑）
-            console.log('右键功能');
-        } else if (event.button === 2) {
-            // 右键操作（在这里执行左键操作的逻辑）
-            console.log('左键功能');
-        }
-    }
-});
