45b89f3088a1fc6ef71849ee63122eaa6790ddad
beetwnus <131743022+beetwnus@users.noreply.github.com>
2023-07-28 19:26:36 +0800

    Add files via upload

--- imgsave.js
+++ imgsave.js
@@ -1,23 +1,20 @@
 function screenshot() {
     // 隱藏按鈕
-    var switchLabel = document.querySelector('.switch');
     var resetButton = document.getElementById('btn_reset');
     var saveButton = document.getElementById('btn_save');
-
-    switchLabel.classList.add('hidden');
+    var dataButton = document.getElementById('btn_data');
     resetButton.classList.add('hidden');
     saveButton.classList.add('hidden');
+    dataButton.classList.add('hidden');
 
     html2canvas(document.getElementById('main')).then(function (canvas) {
         var newTab = window.open();
         newTab.crossOrigin = "anonymous"; // 設定為匿名模式，處理跨域
         newTab.document.write('<img src="' + canvas.toDataURL("image/png") + '" alt="mynikke.png"/>');
+
+        // 截圖完成後顯示按鈕
+        resetButton.classList.remove('hidden');
+        saveButton.classList.remove('hidden');
+        dataButton.classList.remove('hidden');
     });
-
-    // 截圖完成後顯示按鈕
-    switchLabel.classList.remove('hidden');
-    resetButton.classList.remove('hidden');
-    saveButton.classList.remove('hidden');
 }
-
-
