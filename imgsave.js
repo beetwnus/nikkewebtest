9c5322a4d3e5b1eeafaa9254e9017a18007b39d0
beetwnus <131743022+beetwnus@users.noreply.github.com>
2023-07-28 16:15:48 +0800

    Add files via upload

--- imgsave.js
+++ imgsave.js
@@ -1,8 +1,18 @@
-function screenshot(){
-    html2canvas(document.getElementById('main')).then(function(canvas) {
+function screenshot() {
+    // 隱藏按鈕
+    var resetButton = document.getElementById('btn_reset');
+    var saveButton = document.getElementById('btn_save');
+    resetButton.classList.add('hidden');
+    saveButton.classList.add('hidden');
+
+    html2canvas(document.getElementById('main')).then(function (canvas) {
         var newTab = window.open();
         newTab.crossOrigin = "anonymous"; // 設定為匿名模式，處理跨域
         newTab.document.write('<img src="' + canvas.toDataURL("image/png") + '" alt="mynikke.png"/>');
     });
+
+    // 截圖完成後顯示按鈕
+    resetButton.classList.remove('hidden');
+    saveButton.classList.remove('hidden');
 }
 
